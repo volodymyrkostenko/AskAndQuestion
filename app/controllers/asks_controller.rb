64bf8263b821056369be496de559b47026e591a4
vova <vova@vova.vova>
2018-04-17 09:44:45 +0300

    updates ask controller

--- app/controllers/asks_controller.rb
+++ app/controllers/asks_controller.rb
@@ -11,63 +11,67 @@ class AsksController < ApplicationController
 		
 	end
 	def show
-		@answers = @ask.answers
-		
-		@answer= Answer.new
+		validate_current('see') do
+			@answers = @ask.answers	
+			@answer= Answer.new
+		end
 		
 	end
 
-	def new
-		@ask= Ask.new
+	def new 
+		check_users {@ask= Ask.new}
 	end
 	def create
-		@ask = current_user.asks.build(ask_params)
-		respond_to do |format|
-			if @ask.save
-				format.html{redirect_to @ask, notice: "Question was create"}
-			else
-				format.html{render :new, notice: "Something is wrong"}
+		check_users do
+			@ask = current_user.asks.build(ask_params)
+			respond_to do |format|
+				if @ask.save
+					format.html{redirect_to @ask, notice: "Question was create"}
+				else
+					format.html{render :new, notice: "Something is wrong"}
+				end
 			end
-
-
+			
 		end
 	end
 	def edit
-		
+		validate_current('edit')
 	end
 
 	def update
-		respond_to do |format|
-			if @ask.update(ask_params)
-				format.html{redirect_to @ask, notice: "Question was update"}
-			else
-				format.html{render :new, notice: "Something is wrong"}
+		validate_current ('update') do
+			respond_to do |format|
+				if @ask.update(ask_params)
+					format.html{redirect_to @ask, notice: "Question was update"}
+				else
+					format.html{render :new, notice: "Something is wrong"}
+				end
 			end
-
 
 		end
 	end
+		
+		
+	end
 	def destroy
-		@ask.destroy
-		respond_to do |format|
+		validate_current do
+			@ask.destroy
+			respond_to do |format|
 			format.html{redirect_to asks_path, notice: "Your question was destroy"}
 	
 		end
+		end
+		
 	end
-	protected
 
-	def resource_not_found
-      message = "The article you are looking for could not be found"
-      flash[:alert] = message
-      redirect_to root_path
-	end
+	
 	private
 	def set_ask
 		@ask=Ask.find(params[:id])
 		
 	end
 	def ask_params
-		params.require(:ask).permit(:question)		
+		params.require(:ask).permit(:question, :consultant_id)		
 	end
 	def redirect_to_sign_in
 		unless current_consultant || current_user
@@ -75,4 +79,31 @@ class AsksController < ApplicationController
 		end
 		
 	end
-end
+	def validate_current(action)
+		if current_user == @ask.user 
+			yield unless action == 'edit'
+		elsif action == "see" && current_consultant == @ask.consultant && current_consultant != nil
+
+			yield
+		else
+			respond_to do |format|
+				format.html{redirect_to root_path, notice: "You can't #{action} asks other users"}
+			end
+
+		end
+	end
+
+	def check_users
+		if current_user
+			yield
+		else
+			flash[:alert] = "Consultants can't create asks"
+			redirect_to root_path 
+			
+		end
+	end
+	def resource_not_found
+      message = "The article you are looking for could not be found"
+      flash[:alert] = message
+      redirect_to root_path
+	end
