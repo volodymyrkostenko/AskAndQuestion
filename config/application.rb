8fe85d599fc91a071bb087b1a45255bd44ae4fbf
vova <vova@vova.vova>
2018-04-20 12:51:02 +0300

    update key and dwclared rby version

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module AskAndQuestion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
